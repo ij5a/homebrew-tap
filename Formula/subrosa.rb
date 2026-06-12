0811dbfb68c97da52eb9790daf05eb0493d965ab
ij5a <22461427+ij5a@users.noreply.github.com>
2026-06-12 21:28:36 +0800

    subrosa 0.3.0

--- Formula/subrosa.rb
+++ Formula/subrosa.rb
@@ -1,26 +1,26 @@
 class Subrosa < Formula
   desc "Persistent, private memory for Claude Code"
   homepage "https://github.com/ij5a/subrosa"
-  version "0.2.1"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.2.1/subrosa-v0.2.1-aarch64-apple-darwin.tar.gz"
-      sha256 "720d69f34a709daad07985d454d17eae79f314ee8f3b03e1aee83f34699639f7"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.3.0/subrosa-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "b0a760e52af7e23877f06b95387a28fd023b962817f6508a511d42c628699911"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.2.1/subrosa-v0.2.1-x86_64-apple-darwin.tar.gz"
-      sha256 "7dbc89040706928f5c88176d932b3af15dbc7f27d30a8a98dd67b98901c2451d"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.3.0/subrosa-v0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "eb7476259a6eacceda9e7e7aece8aec813127d81c56a27cc1982454c4c56138f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.2.1/subrosa-v0.2.1-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "516e2ef454130bdaaafaab900ce46bcf6240d2f92cd5799309c5bdb3932fa494"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.3.0/subrosa-v0.3.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "302a908eddf581ce9c4b8df2233ffc3ee4eaf1a3815740639818381b579d110b"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.2.1/subrosa-v0.2.1-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "aaeb213f13a25d4c96daa4754523479b68b95d1792d996ff0a86b51c5b0162c6"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.3.0/subrosa-v0.3.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "d3e281ff2908c72441ece4a80e1d2642bd4386e24a97a33d0747d7dd9c760922"
     end
   end
 
