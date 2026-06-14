26914c99d490dafe743c4839adc27e89876207b4
ij5a <22461427+ij5a@users.noreply.github.com>
2026-06-15 02:11:47 +0800

    subrosa 0.9.0

--- Formula/subrosa.rb
+++ Formula/subrosa.rb
@@ -1,26 +1,26 @@
 class Subrosa < Formula
   desc "Persistent, private memory for Claude Code"
   homepage "https://github.com/ij5a/subrosa"
-  version "0.8.1"
+  version "0.9.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.8.1/subrosa-v0.8.1-aarch64-apple-darwin.tar.gz"
-      sha256 "97b5c27f1e6c8a7e9701e5ff9dab5f265de05d26366459f42486ea4ed28a4b49"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.9.0/subrosa-v0.9.0-aarch64-apple-darwin.tar.gz"
+      sha256 "6b87be21fd38da9e499d76a9cb0c3b8248888aa846daf43ea1ecc2a93efb397d"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.8.1/subrosa-v0.8.1-x86_64-apple-darwin.tar.gz"
-      sha256 "87550e0af010587067a66d40f2e95bb44b808c9a8fe67512a48d4de31ea5bae7"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.9.0/subrosa-v0.9.0-x86_64-apple-darwin.tar.gz"
+      sha256 "ebc812011ddb67ae26091c44d173f49d0da2c94bf1cc291f621c64bee1505820"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.8.1/subrosa-v0.8.1-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "6284b3ba04c4fc867ed3321dd5a1d9e3e210e9b194e4026534e150d6b71a3151"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.9.0/subrosa-v0.9.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "de6710eac4ee6000fa5ce5741e916ea547930bf639dbf5e899c6adaf106069f8"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.8.1/subrosa-v0.8.1-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "20d5af8265a406fe8dd673bb775f915d1c80e18523f48bb06f7a75d9d9d9d8b9"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.9.0/subrosa-v0.9.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "060d7c762993115f021ba44a981cebb7e6e60229bb9c0860de50a0ec9510a5dd"
     end
   end
 
