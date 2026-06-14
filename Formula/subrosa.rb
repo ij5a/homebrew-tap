8e3e4616f99458db041f9cc12d0a7c3e9645c500
ij5a <22461427+ij5a@users.noreply.github.com>
2026-06-14 22:35:25 +0800

    subrosa 0.8.1

--- Formula/subrosa.rb
+++ Formula/subrosa.rb
@@ -1,26 +1,26 @@
 class Subrosa < Formula
   desc "Persistent, private memory for Claude Code"
   homepage "https://github.com/ij5a/subrosa"
-  version "0.8.0"
+  version "0.8.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.8.0/subrosa-v0.8.0-aarch64-apple-darwin.tar.gz"
-      sha256 "4da63436c293a6d4d555e5710730580eb613b786d076165317c243a108e20a3d"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.8.1/subrosa-v0.8.1-aarch64-apple-darwin.tar.gz"
+      sha256 "97b5c27f1e6c8a7e9701e5ff9dab5f265de05d26366459f42486ea4ed28a4b49"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.8.0/subrosa-v0.8.0-x86_64-apple-darwin.tar.gz"
-      sha256 "fcc6f9473bd3cb390657869c9fbf0a6301f17c7967d1806a1053dc0e24d254f9"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.8.1/subrosa-v0.8.1-x86_64-apple-darwin.tar.gz"
+      sha256 "87550e0af010587067a66d40f2e95bb44b808c9a8fe67512a48d4de31ea5bae7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.8.0/subrosa-v0.8.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "0f04076fa95358bbf47ccfb91bc7306628796ec9928d79c07b75976428e77183"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.8.1/subrosa-v0.8.1-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "6284b3ba04c4fc867ed3321dd5a1d9e3e210e9b194e4026534e150d6b71a3151"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.8.0/subrosa-v0.8.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "9551fe3fa426050a57380913c0051f43bdab83b243372de1c70c6666b0492dac"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.8.1/subrosa-v0.8.1-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "20d5af8265a406fe8dd673bb775f915d1c80e18523f48bb06f7a75d9d9d9d8b9"
     end
   end
 
