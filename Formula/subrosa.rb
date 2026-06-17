22313dab625f04bcd45122685985864c7fafbc44
ij5a <22461427+ij5a@users.noreply.github.com>
2026-06-17 15:28:18 +0800

    subrosa 0.13.0

--- Formula/subrosa.rb
+++ Formula/subrosa.rb
@@ -1,26 +1,26 @@
 class Subrosa < Formula
   desc "Persistent, private memory for Claude Code"
   homepage "https://github.com/ij5a/subrosa"
-  version "0.12.1"
+  version "0.13.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.12.1/subrosa-v0.12.1-aarch64-apple-darwin.tar.gz"
-      sha256 "da0e06738954a2757478d75d8fec0dbcd9707e37c5e0f970c39bed5c5b7d9711"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.13.0/subrosa-v0.13.0-aarch64-apple-darwin.tar.gz"
+      sha256 "2e4b6ec9c5b5601d9cd7caf164521abdb5d1187e1529228a2aced154419c8b9a"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.12.1/subrosa-v0.12.1-x86_64-apple-darwin.tar.gz"
-      sha256 "6d957e798e2726de2f8db02e60fa70f001b2c4b18af9cc7a22fe61250e79c91e"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.13.0/subrosa-v0.13.0-x86_64-apple-darwin.tar.gz"
+      sha256 "9761177e87ad0f7242c8db00d60009dcd640fb4841e8372caff9efd54f8579e6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.12.1/subrosa-v0.12.1-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "08afd092cea3db3fd258dd18ef9c1141d8b4913aa8573b1d28877de6d7093a68"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.13.0/subrosa-v0.13.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "cbc3c22da82ecdd9e7a7d3ce497ab071688f6fdf32f141ea65cbbdc0f8ae7104"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.12.1/subrosa-v0.12.1-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "7e3de092d2161f758293d6be8339f881f9eec0b44e64631e89da91c0efdc0dbf"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.13.0/subrosa-v0.13.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "95bf9a9bec6f56045393ea0d43a541a2b7607d644751a303584e50a744a25e98"
     end
   end
 
