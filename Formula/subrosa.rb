d989ca44d42b1707ae50e82ba34019812e713564
ij5a <22461427+ij5a@users.noreply.github.com>
2026-06-19 09:43:58 +0800

    subrosa 0.14.0

--- Formula/subrosa.rb
+++ Formula/subrosa.rb
@@ -1,26 +1,26 @@
 class Subrosa < Formula
   desc "Persistent, private memory for Claude Code"
   homepage "https://github.com/ij5a/subrosa"
-  version "0.13.0"
+  version "0.14.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.13.0/subrosa-v0.13.0-aarch64-apple-darwin.tar.gz"
-      sha256 "2e4b6ec9c5b5601d9cd7caf164521abdb5d1187e1529228a2aced154419c8b9a"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.14.0/subrosa-v0.14.0-aarch64-apple-darwin.tar.gz"
+      sha256 "54a7026ba34a96d76cb865b020910d1d398da11c1cc21adc71a2ce9dd03d94f2"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.13.0/subrosa-v0.13.0-x86_64-apple-darwin.tar.gz"
-      sha256 "9761177e87ad0f7242c8db00d60009dcd640fb4841e8372caff9efd54f8579e6"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.14.0/subrosa-v0.14.0-x86_64-apple-darwin.tar.gz"
+      sha256 "02c59913e31e165236770e683a9cdb9e3bf86808ae712a7b5324b1acebd28f1b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ij5a/subrosa/releases/download/v0.13.0/subrosa-v0.13.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "cbc3c22da82ecdd9e7a7d3ce497ab071688f6fdf32f141ea65cbbdc0f8ae7104"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.14.0/subrosa-v0.14.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "98cfc52c6c10c21d1ac2f14414e963cbf48b4c1dceab666d10d61a643d184d76"
     else
-      url "https://github.com/ij5a/subrosa/releases/download/v0.13.0/subrosa-v0.13.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "95bf9a9bec6f56045393ea0d43a541a2b7607d644751a303584e50a744a25e98"
+      url "https://github.com/ij5a/subrosa/releases/download/v0.14.0/subrosa-v0.14.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "8d180d91a322f4242bd4e753eecaaac27413f7c004f1cb694fd57bb5e246cf11"
     end
   end
 
